b41fa509940e86cea910a1c75a1de03bff42ca00
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-08-30 19:51:06 -0600

    Completed 'problems' 7

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -172,7 +172,8 @@ each(names, (item, index) => {
 ////////// PROBLEM 7 //////////
 
 /*
-  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and searches for the user with a matching id.
+  Write a function called getUserById that takes in three parameters: an array of objects (users), an id and a callback, and 
+  searches for the user with a matching id.
   When the correct user object is found, invoke the callback with the user object as an argument.
 */
 
@@ -200,15 +201,22 @@ var users = [
 // Do not edit the code above.
 
 // CODE HERE 
-
+const getUserById = (users, id, callback) => {
+	for (i = 0; i < users.length; i++) {
+		if (users[i].id = id) {
+			callback(users[i]);
+			return;
+		}
+	}
+}
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// getUserById(users, '16t', user => {
-//   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
-// })
+getUserById(users, '16t', user => {
+  console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address) 
+})
 
 ////////// CHALLENGE //////////
 
